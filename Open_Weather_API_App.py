f7a78cd08b3a126cddb20cc64e5cef33288062d7
Kooreis <kooreis.official@gmail.com>
2022-11-29 20:00:00 -0600

    In the second update, a function 'get_weather' is defined. This function takes an API key and a city name as arguments, makes a GET request to the Open Weather API, and returns the JSON response.

--- Open_Weather_API_App.py
+++ Open_Weather_API_App.py
@@ -1,4 +1,11 @@
 ```python
-import requests
-import json
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'
+    }
+    response = requests.get(base_url, params=params)
+    return response.json()
 ```
